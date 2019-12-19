b34af53eb2e0cbddf183f48464fcb47792f70b76
Mika Berglund <mika.berglund@meteoriitti.com>
2019-12-19 07:37:11 +0200

    Added Open ID Connect related code to the Startup class. Added documentation for necessary configuration.

--- OidcWeb/Startup.cs
+++ OidcWeb/Startup.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -14,10 +18,54 @@ namespace OidcWeb
     public class Startup
     {
 
+        public Startup(IConfiguration configuration)
+        {
+
+            // Add the configuration file appsettings.local.json, which is not included by default. That
+            // configuration file is not included in the source code repository, but its contents is
+            // documented in the appsettings.local.json.md file, which is located in the root folder
+            // for this web application.
+            this.Configuration = new ConfigurationBuilder()
+                .AddConfiguration(configuration)
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.local.json", true)
+                .Build();
+        }
+
+        private IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                    options.RequireAuthenticatedSignIn = true;
+                })
+                .AddOpenIdConnect(options =>
+                {
+                    this.Configuration.Bind("oidc", options);
+
+                    options.TokenValidationParameters.NameClaimType = "name";
+                    options.TokenValidationParameters.RoleClaimType = "role";
+
+                    // Here you can set other options if you want to hard-code something instead of having it configurable.
+
+                    options.Events.OnTicketReceived = async (context) =>
+                    {
+                        var user = context.Principal;
+                        var identity = user.Identity as ClaimsIdentity;
+
+                        // Here you can connect to other systems and augment the claims of the logged on user,
+                        // if you want to add something that is not available in the AD tenant handling the authentication.
+
+                        await Task.Yield();
+                    };
+                })
+                .AddCookie();
 
             services.AddControllers();
             services.AddRazorPages();
@@ -33,6 +81,9 @@ namespace OidcWeb
 
             app.UseHttpsRedirection();
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# generic-open-id-connect-web-application
+Generic Open ID Connect Web Application
+=======================================
+
 A sample Open ID Connect web application that you can change to use any OIDC authority by just changing the configuration.
+
+The source code contained in the repository supports the blog article [Building a Web Application that Supports both Azure AD and Azure AD B2C](https://mikaberglund.com/2019/12/18/building-a-web-application-that-supports-both-azure-ad-and-azure-ad-b2c) on my blog at [mikaberglund.com](https://mikaberglund.com).
